c9e4955a1a05e8f1fc8e34f9f46c8efb3a89d3d9
Bruno Pedroso <bruno.pedroso@unochapeco.edu.br>
2023-06-28 17:54:00 -0300

    foi

--- src/routes/ordersRoute.js
+++ src/routes/ordersRoute.js
@@ -1,8 +1,8 @@
 import ordersController from "../controllers/ordersController"
 
 export default (app) => {
-	app.post('/orders/persist', ordersController.persist)
-	app.post('/orders/persist/:id', ordersController.persist)
+	app.post('/orders', ordersController.persist)
+	app.post('/orders/:id', ordersController.persist)
 	app.delete('/orders/destroy/:id', ordersController.destroy)
 	app.get('/orders', ordersController.get)
 	app.get('/orders/:id', ordersController.get)
--- src/routes/productsRoute.js
+++ src/routes/productsRoute.js
@@ -1,8 +1,8 @@
 import productsController from "../controllers/productsController"
 
 export default (app) => {
-	app.post('/products/criar', productsController.persist)
-	app.patch('/products/editar/:id', productsController.persist)
+	app.post('/products', productsController.persist)
+	app.patch('/products/:id', productsController.persist)
 	app.delete('/products/destroy/:id', productsController.destroy)
 	app.get('/products', productsController.get)
 	app.get('/products/:id', productsController.get)
